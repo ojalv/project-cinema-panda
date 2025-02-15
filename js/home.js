ef35142ff5bcffa0ed3660828cdf1ffa18412e4a
joanaRomay <joanaromay@gmail.com>
2025-02-15 18:48:01 -0300

    agregué menú hamburguesa, agregué cards, agregué footer

---
+++ js/home.js
@@ -0,0 +1,7 @@
+const menuIcon = document.getElementById("menu-icon");
+const cerrar = document.getElementById("cerrar")
+const abrir = document.getElementById("abrir");      
+    
+menuIcon.addEventListener("click", function () {
+        menuIcon.classList.toggle("active");
+});
