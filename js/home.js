fa4d6e113080c607a7baf09694ffd1a2acba7ab3
joanaRomay <joanaromay@gmail.com>
2025-02-21 12:01:42 -0300

    agregué funcionalidad a slider banner y slider dias de cine

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+//MENÚ HAMBURGUESA
 const menuIcon = document.getElementById("menu-icon");
 const cerrar = document.getElementById("cerrar")
 const abrir = document.getElementById("abrir");      
@@ -5,3 +6,17 @@ const abrir = document.getElementById("abrir");
 menuIcon.addEventListener("click", function () {
         menuIcon.classList.toggle("active");
 });
+
+
+//SLIDER DIAS CINE
+const slider = document.querySelector('.days-wrapper');
+const leftArrow = document.querySelector('.flecha-izquierda');
+const rightArrow = document.querySelector('.flecha-derecha');
+
+    leftArrow.addEventListener('click', () => {
+        slider.scrollLeft -= 150; // Mueve a la izquierda
+    });
+
+    rightArrow.addEventListener('click', () => {
+        slider.scrollLeft += 150; // Mueve a la derecha
+    });
